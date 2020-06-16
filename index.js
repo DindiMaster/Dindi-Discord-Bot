e92468db1073631ed1a21b40f139d570183c974d
DindiMaster <58052036+DindiMaster@users.noreply.github.com>
2020-06-16 21:57:47 +0200

    updated hopefuly fixed

--- index.js
+++ index.js
@@ -9,64 +9,64 @@ client.once("ready", () => {
 client.login(process.env.token);
 
 client.on("message", message => {
-	if(message.content.toLowerCase() === "ping") {
+	if(message.content === "ping") {
 		message.channel.send("Pong!");
 	}
 
-	if(message.content.toLowerCase() === "d!help") {
+	if(message.content === "d!help") {
 		message.member.send(":smile: **FUN** \n d!say \n d!time \n d!randomnumber \n d!howcoolami \n \n :scroll: **INFO** \n d!help \n d!version \n d!discord \n d!creator \n d!logs setup \n \n :wave: **WELCOME & GOODBYE** \n d!join leave setup");
 	}
 
-	if(message.content.toLowerCase() === "d!nuke") {
+	if(message.content === "d!nuke") {
 		message.channel.send("LOL YOU THOUGHT, banned l o l");
 	}
 
-	if(message.content.toLowerCase() === "Hi") {
+	if(message.content === "Hi") {
 		message.channel.send("Yo, hi!");
 	}
 
-	if(message.content.toLowerCase() === "How are you?") {
+	if(message.content === "How are you?") {
 		message.channel.send("Im very electronic, hbu?");
 	}
 
-	if(message.content.toLowerCase() === "Im good" || message.content === "Im awesome" || message.content === "Im great") {
+	if(message.content === "Im good" || message.content === "Im awesome" || message.content === "Im great") {
 		message.channel.send(":O Thats great!");
 	}
 
-	if(message.content.toLowerCase() === "Im bad" || message.content === "Im sad" || message.content === "Im auful") {
+	if(message.content === "Im bad" || message.content === "Im sad" || message.content === "Im auful") {
 		message.channel.send("Oh no! Now im sad :crying_cat_face:");
 	}
 
-	if(message.conten.toLowerCase() === "d!creator") {
+	if(message.conten === "d!creator") {
 		message.channel.send("This bot is made by **Dindi**, you can visit his youtube channel on https://www.youtube.com/channel/UCjqnUsIVtXHGyCd3q_qvqYQ");
 	}
 
-	if(message.content.toLowerCase() === "d!discord") {
+	if(message.content === "d!discord") {
 		message.channel.send("You can visit the official Dindi Bot disord server on https://discord.gg/NddGpqR");
 	}
 
-	if(message.content.toLowerCase() === "d!join leave setup") {
+	if(message.content === "d!join leave setup") {
 		message.channel.send("Please create a new channel under the name welcome-goodbye, when you are done the bot will automaticly be sending welcome and goodbye messages to that channel. If it doesn't, check if the channel name is correct!");
 
 	}
 
-	if(message.content.toLowerCase() === "d!version") {
+	if(message.content === "d!version") {
 		message.channel.send("The current version is **0.1.2**");
 	}
 
-	if(message.content.toLowerCase() === "d!logs setup") {
+	if(message.content === "d!logs setup") {
 		message.channel.send("Create a channel named logs, the bot should automatically start sending edit/delted messages logs. If it doesn't, check if the channel name is correct.");
 	}
 
-	if(message.content.toLowerCase() === "d!time") {
+	if(message.content === "d!time") {
 		message.channel.send((new Date()).toString);
 	}
 
-	if(message.content.toLowerCase() === "d!randomnumber") {
+	if(message.content === "d!randomnumber") {
 		message.channel.send(Math.floor(Math.random() * 1000000 + 1));
 	}
 
-	if(message.content.toLowerCase() === "d!howcoolami") {
+	if(message.content === "d!howcoolami") {
 		coolness = (Math.floor(Math.random() * 10 + 1));
 		message.channel.send("You are" + coolness + "percent cool");
 	}
@@ -122,9 +122,6 @@ client.on("messageUpdate", async(oldMessage, newMessage) => {
 })
 
 client.on("messageDelete", async message => {
-	if(oldMessage.content === newMessage.content){
-		return;
-	}
 	const logChannel = member.guild.channels.cache.find(channel => channel.name === "logs");
 	var logEmbed = new Discord.RichEmbed()
 	.setAuthor(oldMessage.setAuthor.tag, oldMessage.author.avatarURL)
