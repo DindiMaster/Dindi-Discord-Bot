1853380ad06edd7f283a49913c98aa34c2753fb2
DindiMaster <58052036+DindiMaster@users.noreply.github.com>
2020-06-17 13:19:11 +0200

    hAHA AGSADGSA

--- index.js
+++ index.js
@@ -154,8 +154,8 @@ client.on("message", message => {
 
 		message.delete();
 		message.channel.send(pollembed).then(messageReaction => {
-			message.channel.react("👍");
-			message.channel.react("👎");
+			channel.message.react("👍");
+			channel.message.react("👎");
 			message.delete({ timeout: 1000 });
 		});
 	}
